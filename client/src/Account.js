18ccf652337857b38aa9e6941a2eeab0d4d880b8
45631 <uvilevy@gmail.com>
2019-08-18 21:58:12 +0300

    fetch to state ex.

--- client/src/Account.js
+++ client/src/Account.js
@@ -1,11 +1,28 @@
 import React, { Component } from "react";
 
 export class Account extends Component {
-  render() {
-    return (
-      <div>
-        <p>Account</p>
-      </div>
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3000/users").then(data =>
+      data.json().then(data => this.setState({ users: data }))
     );
+  }
+
+  render() {
+    const users = this.state.users;
+    console.log(users);
+    const usersViews = [];
+    if (users) {
+      users.forEach(user => {
+        const view = <li>{user.name}</li>;
+        usersViews.push(view);
+      });
+    }
+
+    return <div>{usersViews}</div>;
   }
 }
