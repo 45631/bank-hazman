10e0f9da6b690c90f38444a7df6d4f9d7db36b77
45631 <uvilevy@gmail.com>
2019-08-18 20:34:07 +0300

    init client

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+export class App extends Component{
+
+    componentDidMount(){
+        fetch('http://localhost:3000/users').then(data => data.json().then(data => console.log(data)));
+    }
+
+    render(){
+        const categoreis = ["garden", "baby", "car", "food","learn"];
+        const categoriesViews = [];
+        categoreis.forEach((category)=>{
+            const view = <div className="category">{category}</div>;
+            categoriesViews.push(view);
+        });
+        return (
+            <div className="container">
+            <header>
+            <nav>
+            <a href="#"><button>לחשבון שלי</button></a>
+            <ul>
+            <li>בית</li>
+            <li>אודות</li>
+            <li>צור קשר</li>
+            <li>טופס הצטרפות</li>
+            </ul>        
+            </nav>         
+            </header>
+
+                <div>{categoriesViews}</div>
+            </div>
+        );
+    }
+
+}
