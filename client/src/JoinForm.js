f4c05f0d488837d85f840ef4b6b09adeb9c1a222
45631 <uvilevy@gmail.com>
2019-09-19 15:03:37 +0300

    add css

--- client/src/JoinForm.js
+++ client/src/JoinForm.js
@@ -87,138 +87,144 @@ export class JoinForm extends Component {
           <div className="clockimg"></div>
           <div className="clockimg"></div>
         </header>
-        <h3>ברוך הבא לבנק הזמן</h3>
-        <div className="form-group">
-          <p>
-            להצטרפות נא מלא את פרטיך וסמן בתיבות הסימון ליד התחומים בהם הנך
-            מעוניין לתרום
-          </p>
-          <label>
-            שם
-            <input
-              type="text"
-              name="name"
-              value={this.state.name}
-              onChange={this.onNameChange}
-            />
-          </label>
-
-          <label>
-            שם משתמש (יוצג באתר)
-            <input
-              type="text"
-              name="userName"
-              value={this.state.userName}
-              onChange={this.onNameChange}
-            />
-          </label>
-          <label>
-            כתובת דוא"ל
-            <input
-              type="text"
-              name="mail"
-              value={this.state.mail}
-              onChange={this.onNameChange}
-            />
+        <div className="container">
+          <h3>ברוך הבא לבנק הזמן</h3>
+          <div className="form-group">
+            <p>
+              להצטרפות נא מלא את פרטיך וסמן בתיבות הסימון ליד התחומים בהם הנך
+              מעוניין לתרום
+            </p>
             <label>
-              מס. טלפון
+              שם
               <input
                 type="text"
-                name="phone"
-                value={this.state.phone}
+                name="name"
+                value={this.state.name}
                 onChange={this.onNameChange}
               />
             </label>
-          </label>
-          <label>
-            עיר מגורים
-            <input
-              type="text"
-              name="city"
-              value={this.state.city}
-              onChange={this.onNameChange}
-            />
-          </label>
+
+            <label>
+              שם משתמש (יוצג באתר)
+              <input
+                type="text"
+                name="userName"
+                value={this.state.userName}
+                onChange={this.onNameChange}
+              />
+            </label>
+            <label>
+              כתובת דוא"ל
+              <input
+                type="text"
+                name="mail"
+                value={this.state.mail}
+                onChange={this.onNameChange}
+              />
+              <label>
+                מס. טלפון
+                <input
+                  type="text"
+                  name="phone"
+                  value={this.state.phone}
+                  onChange={this.onNameChange}
+                />
+              </label>
+            </label>
+            <label>
+              עיר מגורים
+              <input
+                type="text"
+                name="city"
+                value={this.state.city}
+                onChange={this.onNameChange}
+              />
+            </label>
+          </div>
+          <hr />
+          <div className="domain">
+            <label>
+              <input
+                type="checkbox"
+                name="baby"
+                checked={this.state.domain}
+                onChange={this.onNameChange}
+              />
+              שמרטפות
+              <br />
+              <input
+                type="checkbox"
+                name="tv"
+                checked={this.state.domain}
+                onChange={this.onNameChange}
+              />
+              טכנולוגיה
+              <br />
+              <input
+                type="checkbox"
+                name="pen"
+                checked={this.state.domain}
+                onChange={this.onNameChange}
+              />
+              כתיבה
+              <br />
+              <input
+                type="checkbox"
+                name="motorcycle"
+                checked={this.state.domain}
+                onChange={this.onNameChange}
+              />
+              שליחויות
+              <br />
+              <input
+                type="checkbox"
+                name="cap"
+                checked={this.state.domain}
+                onChange={this.onNameChange}
+              />
+              לימודי עזר
+              <br />
+              <input
+                type="checkbox"
+                name="dog"
+                checked={this.state.domain}
+                onChange={this.onNameChange}
+              />
+              דוגיסיטר
+              <br />
+              <input
+                type="checkbox"
+                name="car"
+                checked={this.state.domain}
+                onChange={this.onNameChange}
+              />
+              הסעות
+              <br />
+              <input
+                type="checkbox"
+                name="hamburger"
+                checked={this.state.domain}
+                onChange={this.onNameChange}
+              />
+              בישולים
+              <br />
+              <input
+                type="checkbox"
+                name="tools"
+                checked={this.state.domain}
+                onChange={this.onNameChange}
+              />
+              עבודות תחזוקה <br />
+            </label>
+          </div>{" "}
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={this.submit}
+          >
+            submit
+          </button>
         </div>
-        <hr />
-        <div className="domain">
-          <label>
-            <input
-              type="checkbox"
-              name="baby"
-              checked={this.state.domain}
-              onChange={this.onNameChange}
-            />
-            שמרטפות
-            <br />
-            <input
-              type="checkbox"
-              name="tv"
-              checked={this.state.domain}
-              onChange={this.onNameChange}
-            />
-            טכנולוגיה
-            <br />
-            <input
-              type="checkbox"
-              name="pen"
-              checked={this.state.domain}
-              onChange={this.onNameChange}
-            />
-            כתיבה
-            <br />
-            <input
-              type="checkbox"
-              name="motorcycle"
-              checked={this.state.domain}
-              onChange={this.onNameChange}
-            />
-            שליחויות
-            <br />
-            <input
-              type="checkbox"
-              name="cap"
-              checked={this.state.domain}
-              onChange={this.onNameChange}
-            />
-            לימודי עזר
-            <br />
-            <input
-              type="checkbox"
-              name="dog"
-              checked={this.state.domain}
-              onChange={this.onNameChange}
-            />
-            דוגיסיטר
-            <br />
-            <input
-              type="checkbox"
-              name="car"
-              checked={this.state.domain}
-              onChange={this.onNameChange}
-            />
-            הסעות
-            <br />
-            <input
-              type="checkbox"
-              name="hamburger"
-              checked={this.state.domain}
-              onChange={this.onNameChange}
-            />
-            בישולים
-            <br />
-            <input
-              type="checkbox"
-              name="tools"
-              checked={this.state.domain}
-              onChange={this.onNameChange}
-            />
-            עבודות תחזוקה <br />
-          </label>
-        </div>{" "}
-        <button type="button" className="btn btn-primary" onClick={this.submit}>
-          submit
-        </button>
       </div>
     );
   }
